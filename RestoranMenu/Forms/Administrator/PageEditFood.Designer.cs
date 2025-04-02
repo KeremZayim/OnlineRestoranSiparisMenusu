1cd9b83f9466677b95dcb58153d41080795b86ef
Kerem Zayim <1741kerem@gmail.com>
2025-04-02 15:34:33 +0300

    02.04.2025 Update Ürün Silme ve Filtreleme Eklendi.

--- RestoranMenu/Forms/Administrator/PageEditFood.Designer.cs
+++ RestoranMenu/Forms/Administrator/PageEditFood.Designer.cs
@@ -32,13 +32,21 @@
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PageEditFood));
+            Bunifu.UI.WinForms.BunifuTextBox.StateProperties stateProperties1 = new Bunifu.UI.WinForms.BunifuTextBox.StateProperties();
+            Bunifu.UI.WinForms.BunifuTextBox.StateProperties stateProperties2 = new Bunifu.UI.WinForms.BunifuTextBox.StateProperties();
+            Bunifu.UI.WinForms.BunifuTextBox.StateProperties stateProperties3 = new Bunifu.UI.WinForms.BunifuTextBox.StateProperties();
+            Bunifu.UI.WinForms.BunifuTextBox.StateProperties stateProperties4 = new Bunifu.UI.WinForms.BunifuTextBox.StateProperties();
             this.dgvFoods = new Bunifu.UI.WinForms.BunifuDataGridView();
+            this.tbFoodName = new Bunifu.UI.WinForms.BunifuTextBox();
+            this.bunifuLabel1 = new Bunifu.UI.WinForms.BunifuLabel();
             ((System.ComponentModel.ISupportInitialize)(this.dgvFoods)).BeginInit();
             this.SuspendLayout();
             // 
             // dgvFoods
             // 
             this.dgvFoods.AllowCustomTheming = false;
+            this.dgvFoods.AllowUserToAddRows = false;
+            this.dgvFoods.AllowUserToDeleteRows = false;
             dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(251)))), ((int)(((byte)(255)))));
             dataGridViewCellStyle1.ForeColor = System.Drawing.Color.Black;
             this.dgvFoods.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
@@ -86,21 +94,118 @@
             this.dgvFoods.HeaderBackColor = System.Drawing.Color.DodgerBlue;
             this.dgvFoods.HeaderBgColor = System.Drawing.Color.Empty;
             this.dgvFoods.HeaderForeColor = System.Drawing.Color.White;
-            this.dgvFoods.Location = new System.Drawing.Point(25, 23);
+            this.dgvFoods.Location = new System.Drawing.Point(25, 104);
+            this.dgvFoods.MultiSelect = false;
             this.dgvFoods.Name = "dgvFoods";
+            this.dgvFoods.ReadOnly = true;
             this.dgvFoods.RowHeadersVisible = false;
             this.dgvFoods.RowHeadersWidth = 100;
             this.dgvFoods.RowTemplate.Height = 40;
             this.dgvFoods.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvFoods.Size = new System.Drawing.Size(987, 694);
+            this.dgvFoods.ShowEditingIcon = false;
+            this.dgvFoods.Size = new System.Drawing.Size(987, 613);
             this.dgvFoods.TabIndex = 0;
             this.dgvFoods.Theme = Bunifu.UI.WinForms.BunifuDataGridView.PresetThemes.Light;
+            this.dgvFoods.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvFoods_CellContentClick);
+            // 
+            // tbFoodName
+            // 
+            this.tbFoodName.AcceptsReturn = false;
+            this.tbFoodName.AcceptsTab = false;
+            this.tbFoodName.AnimationSpeed = 200;
+            this.tbFoodName.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.None;
+            this.tbFoodName.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.None;
+            this.tbFoodName.BackColor = System.Drawing.Color.Transparent;
+            this.tbFoodName.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("tbFoodName.BackgroundImage")));
+            this.tbFoodName.BorderColorActive = System.Drawing.Color.DodgerBlue;
+            this.tbFoodName.BorderColorDisabled = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(204)))), ((int)(((byte)(204)))));
+            this.tbFoodName.BorderColorHover = System.Drawing.Color.FromArgb(((int)(((byte)(105)))), ((int)(((byte)(181)))), ((int)(((byte)(255)))));
+            this.tbFoodName.BorderColorIdle = System.Drawing.Color.Silver;
+            this.tbFoodName.BorderRadius = 1;
+            this.tbFoodName.BorderThickness = 1;
+            this.tbFoodName.CharacterCasing = System.Windows.Forms.CharacterCasing.Normal;
+            this.tbFoodName.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.tbFoodName.DefaultFont = new System.Drawing.Font("Segoe UI", 9.25F);
+            this.tbFoodName.DefaultText = "";
+            this.tbFoodName.FillColor = System.Drawing.Color.White;
+            this.tbFoodName.HideSelection = true;
+            this.tbFoodName.IconLeft = null;
+            this.tbFoodName.IconLeftCursor = System.Windows.Forms.Cursors.IBeam;
+            this.tbFoodName.IconPadding = 10;
+            this.tbFoodName.IconRight = null;
+            this.tbFoodName.IconRightCursor = System.Windows.Forms.Cursors.IBeam;
+            this.tbFoodName.Lines = new string[0];
+            this.tbFoodName.Location = new System.Drawing.Point(388, 50);
+            this.tbFoodName.MaxLength = 32767;
+            this.tbFoodName.MinimumSize = new System.Drawing.Size(1, 1);
+            this.tbFoodName.Modified = false;
+            this.tbFoodName.Multiline = false;
+            this.tbFoodName.Name = "tbFoodName";
+            stateProperties1.BorderColor = System.Drawing.Color.DodgerBlue;
+            stateProperties1.FillColor = System.Drawing.Color.Empty;
+            stateProperties1.ForeColor = System.Drawing.Color.Empty;
+            stateProperties1.PlaceholderForeColor = System.Drawing.Color.Empty;
+            this.tbFoodName.OnActiveState = stateProperties1;
+            stateProperties2.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(204)))), ((int)(((byte)(204)))));
+            stateProperties2.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            stateProperties2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            stateProperties2.PlaceholderForeColor = System.Drawing.Color.DarkGray;
+            this.tbFoodName.OnDisabledState = stateProperties2;
+            stateProperties3.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(105)))), ((int)(((byte)(181)))), ((int)(((byte)(255)))));
+            stateProperties3.FillColor = System.Drawing.Color.Empty;
+            stateProperties3.ForeColor = System.Drawing.Color.Empty;
+            stateProperties3.PlaceholderForeColor = System.Drawing.Color.Empty;
+            this.tbFoodName.OnHoverState = stateProperties3;
+            stateProperties4.BorderColor = System.Drawing.Color.Silver;
+            stateProperties4.FillColor = System.Drawing.Color.White;
+            stateProperties4.ForeColor = System.Drawing.Color.Empty;
+            stateProperties4.PlaceholderForeColor = System.Drawing.Color.Empty;
+            this.tbFoodName.OnIdleState = stateProperties4;
+            this.tbFoodName.Padding = new System.Windows.Forms.Padding(3);
+            this.tbFoodName.PasswordChar = '\0';
+            this.tbFoodName.PlaceholderForeColor = System.Drawing.Color.Silver;
+            this.tbFoodName.PlaceholderText = "Ürün Adı";
+            this.tbFoodName.ReadOnly = false;
+            this.tbFoodName.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.tbFoodName.SelectedText = "";
+            this.tbFoodName.SelectionLength = 0;
+            this.tbFoodName.SelectionStart = 0;
+            this.tbFoodName.ShortcutsEnabled = true;
+            this.tbFoodName.Size = new System.Drawing.Size(260, 37);
+            this.tbFoodName.Style = Bunifu.UI.WinForms.BunifuTextBox._Style.Bunifu;
+            this.tbFoodName.TabIndex = 1;
+            this.tbFoodName.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.tbFoodName.TextMarginBottom = 0;
+            this.tbFoodName.TextMarginLeft = 3;
+            this.tbFoodName.TextMarginTop = 0;
+            this.tbFoodName.TextPlaceholder = "Ürün Adı";
+            this.tbFoodName.UseSystemPasswordChar = false;
+            this.tbFoodName.WordWrap = true;
+            this.tbFoodName.TextChanged += new System.EventHandler(this.tbFoodName_TextChanged);
+            // 
+            // bunifuLabel1
+            // 
+            this.bunifuLabel1.AllowParentOverrides = false;
+            this.bunifuLabel1.AutoEllipsis = false;
+            this.bunifuLabel1.AutoSize = false;
+            this.bunifuLabel1.CursorType = System.Windows.Forms.Cursors.Default;
+            this.bunifuLabel1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.bunifuLabel1.Location = new System.Drawing.Point(318, 50);
+            this.bunifuLabel1.Name = "bunifuLabel1";
+            this.bunifuLabel1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.bunifuLabel1.Size = new System.Drawing.Size(64, 37);
+            this.bunifuLabel1.TabIndex = 2;
+            this.bunifuLabel1.Text = "Filtrele:";
+            this.bunifuLabel1.TextAlignment = System.Drawing.ContentAlignment.MiddleLeft;
+            this.bunifuLabel1.TextFormat = Bunifu.UI.WinForms.BunifuLabel.TextFormattingOptions.Default;
             // 
             // PageEditFood
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1034, 737);
+            this.Controls.Add(this.bunifuLabel1);
+            this.Controls.Add(this.tbFoodName);
             this.Controls.Add(this.dgvFoods);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -115,5 +220,7 @@
         #endregion
 
         private Bunifu.UI.WinForms.BunifuDataGridView dgvFoods;
+        private Bunifu.UI.WinForms.BunifuTextBox tbFoodName;
+        private Bunifu.UI.WinForms.BunifuLabel bunifuLabel1;
     }
 }
--- RestoranMenu/Forms/Administrator/PageEditFood.cs
+++ RestoranMenu/Forms/Administrator/PageEditFood.cs
@@ -1,10 +1,12 @@
 ﻿using RestoranMenu.Classes;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Data.SqlClient;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,39 +16,94 @@ namespace RestoranMenu.Forms.Administrator
 {
     public partial class PageEditFood : Form
     {
+        string query = @"
+                SELECT 
+                    f.food_id, 
+                    f.food_name, 
+                    c.category_name AS Kategori, 
+                    d.diet_type_name AS 'Diyet Tipi', 
+                    f.food_calorie, 
+                    f.food_picture, 
+                    f.food_price 
+                FROM foods f
+                LEFT JOIN categories c ON f.category_id = c.category_id
+                LEFT JOIN diet_types d ON f.diet_type_id = d.diet_type_id";
         public PageEditFood()
         {
             InitializeComponent();
-            VerileriYukle();
+            VerileriYukle(query);
             dgvFoods.RowTemplate.Height = 100;
+            dgvFoods.CellPainting += dgvFoods_CellPainting; // Resim çizmek için event ekle
         }
-        private void VerileriYukle()
+        // Resimi Stretch Yapmak İçin
+        private void dgvFoods_CellPainting(object sender, DataGridViewCellPaintingEventArgs e)
         {
+            if (dgvFoods.Columns[e.ColumnIndex].Name == "food_picture" && e.RowIndex >= 0)
+            {
+                e.Paint(e.CellBounds, DataGridViewPaintParts.All & ~DataGridViewPaintParts.ContentForeground);
+                if (dgvFoods.Rows[e.RowIndex].Cells["food_picture"].Value != DBNull.Value)
+                {
+                    try
+                    {
+                        byte[] imageData = (byte[])dgvFoods.Rows[e.RowIndex].Cells["food_picture"].Value;
+                        using (MemoryStream ms = new MemoryStream(imageData))
+                        {
+                            Image img = Image.FromStream(ms);
+                            Rectangle rect = e.CellBounds;
+                            rect.Inflate(-5, -5); // Kenarlardan biraz boşluk bırak
+                            e.Graphics.DrawImage(img, rect);
+                        }
+                    }
+                    catch
+                    {
+                        e.Graphics.DrawString("Resim Yok", dgvFoods.Font, Brushes.Red, e.CellBounds.X + 10, e.CellBounds.Y + 30);
+                    }
+                }
+                else
+                {
+                    e.Graphics.DrawString("Resim Yok", dgvFoods.Font, Brushes.Red, e.CellBounds.X + 10, e.CellBounds.Y + 30);
+                }
+                e.Handled = true;
+            }
+        }
+        private void VerileriYukle(string query)
+        {
+            dgvFoods.Columns.Clear();
             using (SqlConnection baglanti = new SqlConnection(SqlServer.ConnectionString))
             {
                 try
                 {
                     baglanti.Open();
-                    string query = "SELECT * FROM foods";  // Tablondaki verileri çekiyoruz
+
                     SqlDataAdapter adapter = new SqlDataAdapter(query, baglanti);
                     DataTable dt = new DataTable();
                     adapter.Fill(dt);
                     dgvFoods.DataSource = dt;  // DataGridView'e bağla
 
+                    // Çarpı butonunu eklemek için yeni bir sütun ekleyelim
+                    DataGridViewButtonColumn deleteColumn = new DataGridViewButtonColumn();
+                    deleteColumn.Name = "Sil";
+                    deleteColumn.HeaderText = "Sil";
+                    deleteColumn.Width = 50;
+                    deleteColumn.Text = "❌";
+                    deleteColumn.UseColumnTextForButtonValue = true;
+                    dgvFoods.Columns.Add(deleteColumn);
+
                     // Sütun başlıklarını belirleme
                     dgvFoods.Columns["food_id"].HeaderText = "Gıda ID";
                     dgvFoods.Columns["food_name"].HeaderText = "Gıda Adı";
-                    dgvFoods.Columns["category_id"].HeaderText = "Kategori ID";
-                    dgvFoods.Columns["diet_type_ID"].HeaderText = "Diyet Tipi ID";
-                    dgvFoods.Columns["category_id"].HeaderText = "Kategori ID";
+                    dgvFoods.Columns["Kategori"].HeaderText = "Kategori";
+                    dgvFoods.Columns["Diyet Tipi"].HeaderText = "Diyet Tipi";
+                    dgvFoods.Columns["food_calorie"].HeaderText = "Kalori";
+                    dgvFoods.Columns["food_picture"].HeaderText = "Resim";
                     dgvFoods.Columns["food_price"].HeaderText = "Fiyat";
 
-                    // Sütun genişliklerini 50 olarak ayarla
+                    // Sütun genişlikleri
                     dgvFoods.Columns["food_id"].Width = 70;
-                    dgvFoods.Columns["food_name"].Width = 250;
-                    dgvFoods.Columns["category_id"].Width = 110;
-                    dgvFoods.Columns["diet_type_id"].Width = 110;
-                    dgvFoods.Columns["food_calorie"].Width = 110;
+                    dgvFoods.Columns["food_name"].Width = 200;
+                    dgvFoods.Columns["Kategori"].Width = 150;
+                    dgvFoods.Columns["Diyet Tipi"].Width = 150;
+                    dgvFoods.Columns["food_calorie"].Width = 100;
                     dgvFoods.Columns["food_picture"].Width = 170;
                     dgvFoods.Columns["food_price"].Width = 100;
                 }
@@ -54,6 +111,126 @@ namespace RestoranMenu.Forms.Administrator
                 {
                     MessageBox.Show("Hata: " + ex.Message);
                 }
+            }
+        }
+        private void dgvFoods_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == dgvFoods.Columns["Sil"].Index && e.RowIndex >= 0)
+            {
+                try
+                {
+                    string foodNameValue = dgvFoods.Rows[e.RowIndex].Cells["food_name"].Value?.ToString().Trim();
+
+                    if (string.IsNullOrEmpty(foodNameValue))
+                    {
+                        MessageBox.Show("Gıda adı bulunamadı!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+
+                    DialogResult result = MessageBox.Show($"'{foodNameValue}' adlı ürünü silmek istediğinizden emin misiniz?",
+                                                          "Silme Onayı", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+                    if (result == DialogResult.Yes)
+                    {
+                        DeleteFood(foodNameValue);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Hata: " + ex.Message);
+                }
+            }
+        }
+
+        private void DeleteFood(string foodName)
+        {
+            using (SqlConnection baglanti = new SqlConnection(SqlServer.ConnectionString))
+            {
+                try
+                {
+                    baglanti.Open();
+
+                    // İlk olarak food_id'yi bul
+                    string getFoodIdQuery = "SELECT food_id FROM dbo.foods WHERE food_name = @foodName";
+                    SqlCommand getIdCmd = new SqlCommand(getFoodIdQuery, baglanti);
+                    getIdCmd.Parameters.AddWithValue("@foodName", foodName);
+
+                    object foodIdObj = getIdCmd.ExecuteScalar();
+
+                    if (foodIdObj == null)
+                    {
+                        MessageBox.Show("Ürün bulunamadı!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+
+                    int foodId = Convert.ToInt32(foodIdObj);
+
+                    // Silme işlemleri
+                    string deleteQuery = @"
+                DELETE FROM dbo.foods_allergens WHERE food_id = @foodId;
+                DELETE FROM dbo.order_details WHERE food_id = @foodId;
+                DELETE FROM dbo.foods WHERE food_id = @foodId;
+            ";
+
+                    SqlCommand komut = new SqlCommand(deleteQuery, baglanti);
+                    komut.Parameters.AddWithValue("@foodId", foodId);
+
+                    int affectedRows = komut.ExecuteNonQuery();
+
+                    if (affectedRows > 0)
+                        MessageBox.Show("Ürün başarıyla silindi!", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    else
+                        MessageBox.Show("Ürün silinemedi!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    VerileriYukle(query);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Hata: " + ex.Message);
+                }
+            }
+        }
+
+
+        private void tbFoodName_TextChanged(object sender, EventArgs e)
+        {
+            if (tbFoodName.Text == "" || tbFoodName.Text == null)
+            {
+                query = @"
+                SELECT 
+                    f.food_id, 
+                    f.food_name, 
+                    c.category_name AS Kategori, 
+                    d.diet_type_name AS 'Diyet Tipi', 
+                    f.food_calorie, 
+                    f.food_picture, 
+                    f.food_price 
+                FROM foods f
+                LEFT JOIN categories c ON f.category_id = c.category_id
+                LEFT JOIN diet_types d ON f.diet_type_id = d.diet_type_id";
+            }
+            else
+            {
+                query = @"
+                SELECT 
+                    f.food_id, 
+                    f.food_name, 
+                    c.category_name AS Kategori, 
+                    d.diet_type_name AS 'Diyet Tipi', 
+                    f.food_calorie, 
+                    f.food_picture, 
+                    f.food_price 
+                FROM foods f
+                LEFT JOIN categories c ON f.category_id = c.category_id
+                LEFT JOIN diet_types d ON f.diet_type_id = d.diet_type_id
+                WHERE f.food_name COLLATE SQL_Latin1_General_CP1_CI_AS LIKE '%" + tbFoodName.Text + "%'";
+            }
+            try
+            {
+                VerileriYukle(query);
+            }
+            catch (Exception)
+            {
+                throw;
             }
         }
     }
